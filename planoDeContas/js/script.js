578351d30dcd361cc106807652d210179c9874d8
Helder-Machava <98541716+macbench@users.noreply.github.com>
2022-07-06 23:17:25 +0200

    ajust ++

--- planoDeContas/js/script.js
+++ planoDeContas/js/script.js
@@ -1,9 +1,19 @@
 contas = [
 
   "<strong>CLASSE 1 - MEIOS CIRCULANTES</strong>",
-  "1.1 Caixa","<strong>1.2 Bancos</strong>","&nbsp; 1.2.1 Depósitos à ordem","&nbsp; 1.2.2 Depósito com pré-aviso","&nbsp; 1.2.3 Depósitos à prazo",
+  "1.1 Caixa","<strong>1.2 Bancos</strong>","&nbsp;&nbsp; 1.2.1 Depósitos à ordem","&nbsp;&nbsp; 1.2.2 Depósito com pré-aviso","&nbsp;&nbsp; 1.2.3 Depósitos à prazo",
+  
   "<strong>CLASSE 2 - INENTÁRIOS E ACTIVOS BIOLÓGICOS</strong>",
-  "Compras","Mercadorias"
+  "2.1 Compras","2.2 Mercadorias",
+
+  "<strong>CLASSE 3 - INVESTIMENTOS DE CAPITAL</strong>",
+  "<strong>CLASSE 4 - CONTAS A RECEBER, A PAGAR, ACRÉSCIMOS E DIF.</strong>",
+  "<strong>CLASSE 5 - CAPITAL PRÓPRIO</strong>",
+  "<strong>CLASSE 6 - GASTOS E PERDAS</strong>",
+  "<strong>CLASSE 7 - RENDIMENTOS E GANHOS</strong>",
+  "<strong>CLASSE 8 - RESULTADOS</strong>",
+
+
 ]
 
 
