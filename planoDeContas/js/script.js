2eccf9b3be82f565f42e4783e1bbd946c7bcb731
Helder-Machava <98541716+macbench@users.noreply.github.com>
2022-07-06 22:59:53 +0200

    AJUST ON SEARCH

--- planoDeContas/js/script.js
+++ planoDeContas/js/script.js
@@ -1,15 +1,21 @@
-contas = ["Caixa","Bancos","Depósitos à ordem","Depósitos à prazo","Compras","Mercadorias"]
+contas = [
+
+  "<strong>CLASSE 1 - MEIOS CIRCULANTES</strong>",
+  "1.1 Caixa","<strong>1.2 Bancos</strong>","&nbsp; 1.2.1 Depósitos à ordem","&nbsp; 1.2.2 Depósito com pré-aviso","&nbsp; 1.2.3 Depósitos à prazo",
+  "<strong>CLASSE 2 - INENTÁRIOS E ACTIVOS BIOLÓGICOS</strong>",
+  "Compras","Mercadorias"
+]
 
 
 t=""
 
 for(i in contas){
   t+= "<li>"+contas[i]+"</li>"
-  //contas[i] = contas[i].toLowerCase()
+  contas[i] = contas[i].toLowerCase()
 }
 lista.innerHTML = t
 pesquisa.onkeyup=function(e){
-  t = this.value
+  t = this.value.toLowerCase()
   
   r = new RegExp(t,"g")
 
